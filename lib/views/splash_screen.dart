1aa92911582eeffedf3acf1a6cfe753dab7acaab
ahmed-elmalky <ahmedelmalky35@gmail.com>
2024-05-14 21:10:19 +0300

    Finish splash_screen.dart Without navigate to the next Screen.

---
+++ lib/views/splash_screen.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(vsync: this);
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xff222831),
+      body: splash(),
+    );
+  }
+  Widget splash(){
+    return Center(
+      child: Container(
+        child: Lottie.asset(
+          'assets/animations/dice_animation.json',
+        width: 350,
+        height: 350,
+        fit: BoxFit.fill)
+      ),
+    );
+  }
+}
