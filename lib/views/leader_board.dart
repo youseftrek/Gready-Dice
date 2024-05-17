9a1d6f7c7d6c93be445d87eec33d66ce2d875904
YousefTarek <youseftarek.business@gmail.com>
2024-05-17 07:02:20 +0300

    edit: Leader Board Stayles + responsivness🫡

--- lib/views/leader_board.dart
+++ lib/views/leader_board.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:greedy_dice_project/models/api_service_model.dart';
 import 'package:greedy_dice_project/widgets/popup_password.dart';
 
@@ -6,10 +7,9 @@ import '../models/user_model.dart';
 
 class LeaderBoard extends StatefulWidget {
   final User LoggedInuser;
-  const LeaderBoard({super.key,required this.LoggedInuser});
+  const LeaderBoard({super.key, required this.LoggedInuser});
 
   @override
-
   State<LeaderBoard> createState() => _LeaderBoard();
 }
 
@@ -31,18 +31,20 @@ class _LeaderBoard extends State<LeaderBoard> {
       isLoading = false;
     });
   }
+
   void handleTap(int index) {
     var user = users[index];
-    showPasswordDialog(context,user,widget.LoggedInuser);
+    showPasswordDialog(context, user, widget.LoggedInuser);
   }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         elevation: 0,
         centerTitle: true,
-        backgroundColor: Color(0xff222831),
-        title: Text(
+        backgroundColor: const Color(0xff222831),
+        title: const Text(
           "Leaderboard",
           style: TextStyle(
             color: Color(0xffdddddd),
@@ -51,222 +53,210 @@ class _LeaderBoard extends State<LeaderBoard> {
           ),
         ),
         leading: IconButton(
-          color: Color(0xffdddddd),
-          icon: Icon(Icons.arrow_back),
+          color: const Color(0xffdddddd),
+          icon: const Icon(Icons.arrow_back),
           onPressed: () {
             Navigator.of(context)
                 .pop(); // Navigate back when the back button is pressed
           },
         ),
       ),
-      backgroundColor: Color(0xff222831),
-      body: isLoading? Center(child: CircularProgressIndicator()): Stack(
-        children: [
-          Stack(
-            children: [
-              Positioned(
-                child: Column(
-                  children: [
-                    Image.asset(
-                      "assets/images/leaderBoard.png",
-                      fit: BoxFit.cover,
-                    ),
-                  ],
+      backgroundColor: const Color(0xff222831),
+      body: isLoading
+          ? const Center(child: CircularProgressIndicator())
+          : Stack(
+              children: [
+                Align(
+                  alignment: Alignment.topCenter,
+                  child: Image.asset(
+                    "assets/images/leaderBoard.png",
+                    fit: BoxFit.cover,
+                    width: MediaQuery.of(context).size.width,
+                  ),
                 ),
-              ),
-            ],
-          ),
-          Align(
-            alignment: Alignment.bottomCenter,
-            child: Container(
-              height: MediaQuery.of(context).size.height / 2.1,
-              width: MediaQuery.of(context).size.width,
-              decoration: const BoxDecoration(
-                  color: Color(0xff393E46),
-                  borderRadius: BorderRadius.only(
-                      topLeft: Radius.circular(20),
-                      topRight: Radius.circular(25))),
-              child: ListView.builder(
-                  shrinkWrap: true,
-                  physics: BouncingScrollPhysics(),
-                  itemCount: users.length,
-                  itemBuilder: (context, index) {
-                    final items = users;
-                    return GestureDetector(
-                      onTap: () => handleTap(index),
-                      child: Padding(
-                        padding: EdgeInsets.only(right: 15, left: 15, top: 15),
-                        child: Container(
-                          decoration: BoxDecoration(
-                            color: Color(0xff222831),
-                            borderRadius: BorderRadius.circular(50),
-                          ),
-                          height: 50,
-                          child: Row(
-                            children: [
-                              const SizedBox(
-                                width: 5,
+                Align(
+                  alignment: Alignment.bottomCenter,
+                  child: Container(
+                    height: MediaQuery.of(context).size.height / 2.1,
+                    width: MediaQuery.of(context).size.width,
+                    decoration: const BoxDecoration(
+                      color: Color(0xff393E46),
+                      borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(20),
+                        topRight: Radius.circular(25),
+                      ),
+                    ),
+                    child: ListView.builder(
+                      shrinkWrap: true,
+                      physics: const BouncingScrollPhysics(),
+                      itemCount: users.length,
+                      itemBuilder: (context, index) {
+                        final items = users;
+                        return GestureDetector(
+                          onTap: () => handleTap(index),
+                          child: Padding(
+                            padding: const EdgeInsets.symmetric(
+                                horizontal: 15, vertical: 8),
+                            child: Container(
+                              decoration: BoxDecoration(
+                                color: const Color(0xff222831),
+                                borderRadius: BorderRadius.circular(50),
                               ),
-                              CircleAvatar(
-                                radius: 20,
-                                backgroundImage:
-                                    NetworkImage(items[index]['avatar']),
-                              ),
-                              const SizedBox(
-                                width: 10,
-                              ),
-                              Row(
-                                children: [
-                                  Row(
-                                    children: [
-                                      Column(
-                                        mainAxisAlignment:
-                                            MainAxisAlignment.center,
+                              height: 50,
+                              child: Padding(
+                                padding:
+                                    const EdgeInsets.symmetric(horizontal: 5),
+                                child: Row(
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceBetween,
+                                  children: [
+                                    SizedBox(
+                                      width:
+                                          MediaQuery.of(context).size.width / 3,
+                                      child: Row(
                                         children: [
-                                          Text(
-                                            items[index]['name'],
-                                            style: const TextStyle(
+                                          CircleAvatar(
+                                            radius: 20,
+                                            backgroundImage: NetworkImage(
+                                                items[index]['avatar']),
+                                          ),
+                                          Padding(
+                                            padding:
+                                                const EdgeInsets.only(left: 10),
+                                            child: Text(
+                                              items[index]['name'],
+                                              style: const TextStyle(
                                                 color: Color(0xff00ADB5),
                                                 fontSize: 16,
-                                                fontWeight: FontWeight.bold),
-                                          ),
-                                          Text(
-                                            "#${index + 1}",
-                                            style: const TextStyle(
-                                                color: Color(0xffdddddd),
-                                                fontWeight: FontWeight.bold),
+                                                fontWeight: FontWeight.bold,
+                                              ),
+                                            ),
                                           ),
                                         ],
                                       ),
-                                      SizedBox(
-                                        width: 175,
-                                        child: Row(
-                                          mainAxisAlignment:
-                                              MainAxisAlignment.end,
-                                          children: [
-                                            Text(
-                                              (items[index]['score'])
-                                                      .toString() +
-                                                  " ⭐",
-                                              style: TextStyle(
-                                                  color: Color(0xffdddddd),
-                                                  fontWeight: FontWeight.bold),
-                                            ),
-                                          ],
-                                        ),
-                                      )
-                                    ],
-                                  ),
-                                ],
+                                    ),
+                                    Opacity(
+                                      opacity: .2,
+                                      child: Text(
+                                        "#${index + 1}",
+                                        style: const TextStyle(
+                                            color: Color(0xffdddddd),
+                                            fontWeight: FontWeight.bold,
+                                            fontSize: 24),
+                                      ),
+                                    ),
+                                    Text(
+                                      "${items[index]['score']} ⭐",
+                                      style: const TextStyle(
+                                        color: Color(0xffdddddd),
+                                        fontWeight: FontWeight.bold,
+                                      ),
+                                    ),
+                                  ],
+                                ),
                               ),
-                            ],
+                            ),
                           ),
+                        );
+                      },
+                    ),
+                  ),
+                ),
+                Align(
+                  alignment: Alignment.topCenter,
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      const SizedBox(height: 5),
+                      Text(
+                        users.isNotEmpty ? '${users[0]['name']}' : '',
+                        style: const TextStyle(
+                          fontWeight: FontWeight.bold,
+                          color: Color(0xff00ADB5),
+                          fontSize: 16,
                         ),
                       ),
-                    );
-                  }),
-            ),
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Column(
-                children: [
-                  SizedBox(
-                    height: 5,
-                  ),
-                  Text(
-                    '${users[0]['name']}',
-                    style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        color: Color(0xff00ADB5),
-                        fontSize: 16),
-                  ),
-                  Text(
-                    '${users[0]['score']} ⭐',
-                    style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        color: Color(0xffdddddd),
-                        fontSize: 14),
-                  ),
-                  CircleAvatar(
-                    radius: 40,
-                    backgroundImage: NetworkImage(users[0]['avatar']),
-                  ),
-                ],
-              ),
-            ],
-          ),
-          Positioned(
-            top: 40,
-            left: 40,
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Column(
-                  children: [
-                    SizedBox(
-                      height: 5,
-                    ),
-                    Text(
-                      '${users[1]['name']}',
-                      style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                          color: Color(0xff00ADB5),
-                          fontSize: 16),
-                    ),
-                    Text(
-                      '${users[1]['score']} ⭐',
-                      style: TextStyle(
+                      Text(
+                        users.isNotEmpty ? '${users[0]['score']} ⭐' : '',
+                        style: const TextStyle(
                           fontWeight: FontWeight.bold,
                           color: Color(0xffdddddd),
-                          fontSize: 14),
-                    ),
-                    CircleAvatar(
-                      radius: 35,
-                      backgroundImage: NetworkImage(users[1]['avatar']),
-                    ),
-                  ],
+                          fontSize: 14,
+                        ),
+                      ),
+                      CircleAvatar(
+                        radius: 40,
+                        backgroundImage: users.isNotEmpty
+                            ? NetworkImage(users[0]['avatar'])
+                            : null,
+                      ),
+                    ],
+                  ),
                 ),
-              ],
-            ),
-          ),
-          Positioned(
-            top: 40,
-            right: 40,
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Column(
-                  children: [
-                    SizedBox(
-                      height: 5,
-                    ),
-                    Text(
-                      '${users[2]['name']}',
-                      style: TextStyle(
+                Positioned(
+                  top: 40,
+                  left: MediaQuery.of(context).size.width * 0.12,
+                  child: Column(
+                    children: [
+                      const SizedBox(height: 5),
+                      Text(
+                        users.length > 1 ? '${users[1]['name']}' : '',
+                        style: const TextStyle(
                           fontWeight: FontWeight.bold,
                           color: Color(0xff00ADB5),
-                          fontSize: 16),
-                    ),
-                    Text(
-                      '${users[2]['score']} ⭐',
-                      style: TextStyle(
+                          fontSize: 16,
+                        ),
+                      ),
+                      Text(
+                        users.length > 1 ? '${users[1]['score']} ⭐' : '',
+                        style: const TextStyle(
                           fontWeight: FontWeight.bold,
                           color: Color(0xffdddddd),
-                          fontSize: 14),
-                    ),
-                    CircleAvatar(
-                      radius: 35,
-                      backgroundImage: NetworkImage(users[2]['avatar']),
-                    ),
-                  ],
+                          fontSize: 14,
+                        ),
+                      ),
+                      CircleAvatar(
+                        radius: 35,
+                        backgroundImage: users.length > 1
+                            ? NetworkImage(users[1]['avatar'])
+                            : null,
+                      ),
+                    ],
+                  ),
                 ),
+                Positioned(
+                  top: 40,
+                  right: MediaQuery.of(context).size.width * 0.12,
+                  child: Column(
+                    children: [
+                      const SizedBox(height: 5),
+                      Text(
+                        users.length > 2 ? '${users[2]['name']}' : '',
+                        style: const TextStyle(
+                          fontWeight: FontWeight.bold,
+                          color: Color(0xff00ADB5),
+                          fontSize: 16,
+                        ),
+                      ),
+                      Text(
+                        users.length > 2 ? '${users[2]['score']} ⭐' : '',
+                        style: const TextStyle(
+                          fontWeight: FontWeight.bold,
+                          color: Color(0xffdddddd),
+                          fontSize: 14,
+                        ),
+                      ),
+                      CircleAvatar(
+                        radius: 35,
+                        backgroundImage: users.length > 2
+                            ? NetworkImage(users[2]['avatar'])
+                            : null,
+                      ),
+                    ],
+                  ),
+                )
               ],
             ),
-          )
-        ],
-      ),
     );
   }
 }
